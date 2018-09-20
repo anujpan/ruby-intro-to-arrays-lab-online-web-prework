a12be63bc8d6dd1f558b3b888d2871e5ca409536
Anuj Panchal <anujpan1@gmail.com>
2018-09-20 17:44:21 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,6 +5,7 @@ end
 def array_with_two_elements
   my_new_array = []
 
+<<<<<<< HEAD
   my_new_array.push(rand(6))
   my_new_array.push(rand(6))
 end
@@ -44,3 +45,12 @@ def length_of_array(array)
   
   list.size
 end
+=======
+  count = 0
+
+  until count > 2
+    my_new_array.push("anuj")
+    count += 1
+  end
+end
+>>>>>>> a778de654da618f6da6f4c69839ab768a9fdfea3
